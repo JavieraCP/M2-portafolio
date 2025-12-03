283b5e56c968bb95e3fb8297b9563ebc08546d36
JavieraCP <javiera.codoceop@gmail.com>
2025-12-03 15:07:53 -0300

    Add files via upload

---
+++ Modulo 2 Portafolio/javascript/app.js
@@ -0,0 +1,8 @@
+function mostrarDescripcion(id) {
+  var elemento = document.getElementById(id);
+  if (elemento.style.display === "none" || elemento.style.display === "") {
+    elemento.style.display = "block";
+  } else {
+    elemento.style.display = "none";
+  }
+}
